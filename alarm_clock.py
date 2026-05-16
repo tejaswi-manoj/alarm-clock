46b3e31f2755ad266e48f16439d254b57071093c
tejaswi-manoj <tejaswimanoj@gmail.com>
2026-05-16 17:19:39 -0400

    Fix display x3

--- alarm_clock.py
+++ alarm_clock.py
@@ -44,7 +44,7 @@ def animation(device, from_y, to_y):
 def main():
     # Setup for Banggood version of 4 x 8x8 LED Matrix (https://bit.ly/2Gywazb)
     serial = spi(port=0, device=0, gpio=noop())
-    device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=True)
+    device = max7219(serial, cascaded=4, block_orientation=-90, blocks_arranged_in_reverse_order=False)
     device.contrast(16)
 
     # The time ascends from the abyss...
